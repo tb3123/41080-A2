53e0d1da3140f3e3c5424066cdfa270f88fe364f
tb3123 <thomas.bogdanovic@student.uts.edu.au>
2025-10-30 10:52:31 +1100

    making tokenizer

--- Main.cpp
+++ Main.cpp
@@ -4,25 +4,51 @@
 #include <map>
 
 enum class TokenType {
-    NUM,
+    NUMBER,
     ID,
+
     L_PAREN,
     R_PAREN,
+
     PLUS,
+    MINUS,
     MULTIPLY,
-    EQUAL,
+    EQUALS,
+
     IF,
     LAMBDA,
     DEF,
+
     END
 };
 
 class Token {
-private:
-    TokenType type;
-    int value;
 public:
-    Token(TokenType type) : type{type} {}
+    TokenType type;
+    std::string value;
+public:
+    Token(TokenType type) : type{type}, value{""} {}
+    Token(TokenType type, std::string value) : type{type}, value{value} {}
+
+    friend std::ostream& operator<<(std::ostream& os, const Token& token) {
+        switch(token.type) {
+            case TokenType::PLUS:
+            {
+                os << "plus";
+                break;
+            }
+            case TokenType::NUMBER:
+            {
+                os << "number";
+                break;
+            }
+            default:
+            {
+                break;
+            }
+        }
+        return os;
+    }
 };
 
 Token findOp(const char c) {
@@ -30,22 +56,38 @@ Token findOp(const char c) {
         { '+', TokenType::PLUS },
         { '*', TokenType::MULTIPLY },
     };
-    return Token(operatorMap.find(c));
+
+    return Token(operatorMap[c]);
+}
+
+bool digit(char c) {
+    return (c >= 48 && c <= 57);
 }
 
 std::vector<Token> tokenize(std::string str) {
     std::vector<Token> tokens;
 
     for (const char& c : str){
+        if(c == ' ') continue;
+
+        if(digit(c)) {
+            tokens.push_back(Token(TokenType::NUMBER, std::to_string(c)));
+            continue;
+        }
+
         tokens.push_back(findOp(c));
     }
 
-    return std::vector<Token>();
+    return tokens;
 }
 
 int main() {
+    std::string str = "+ 4 + 6";
 
-
+    std::vector<Token> tokens = tokenize(str);
+    for(auto& token : tokens) {
+        std::cout << token << '\n';
+    }
 
     return 0;
 }
