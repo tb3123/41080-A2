00090a91716cd8b06708282e0becc34d10890225
tb3123 <thomas.bogdanovic@student.uts.edu.au>
2025-10-30 11:03:18 +1100

    tokenizer

--- Main.cpp
+++ Main.cpp
@@ -31,10 +31,26 @@ public:
     Token(TokenType type, std::string value) : type{type}, value{value} {}
 
     friend std::ostream& operator<<(std::ostream& os, const Token& token) {
+        os << "{";
         switch(token.type) {
             case TokenType::PLUS:
             {
                 os << "plus";
+                break;
+            }
+            case TokenType::MINUS:
+            {
+                os << "minus";
+                break;
+            }
+            case TokenType::MULTIPLY:
+            {
+                os << "multiply";
+                break;
+            }
+            case TokenType::EQUALS:
+            {
+                os << "equals";
                 break;
             }
             case TokenType::NUMBER:
@@ -44,9 +60,12 @@ public:
             }
             default:
             {
+                os << "unknown";
                 break;
             }
         }
+        os << "}";
+
         return os;
     }
 };
@@ -68,21 +87,46 @@ std::vector<Token> tokenize(std::string str) {
     std::vector<Token> tokens;
 
     for (const char& c : str){
+        // whitespace
         if(c == ' ') continue;
+
+        if(c == '(') {
+            tokens.push_back(Token(TokenType::L_PAREN));
+            continue;
+        }
+        if(c == ')') {
+            tokens.push_back(Token(TokenType::R_PAREN));
+            continue;
+        }
 
         if(digit(c)) {
             tokens.push_back(Token(TokenType::NUMBER, std::to_string(c)));
             continue;
         }
 
-        tokens.push_back(findOp(c));
+        if(c == '+') {
+            tokens.push_back(Token(TokenType::PLUS));
+            continue;
+        }
+        if(c == '-') {
+            tokens.push_back(Token(TokenType::MINUS));
+            continue;
+        }
+        if(c == '*') {
+            tokens.push_back(Token(TokenType::MULTIPLY));
+            continue;
+        }
+        if(c == '=') {
+            tokens.push_back(Token(TokenType::EQUALS));
+            continue;
+        }
     }
 
     return tokens;
 }
 
 int main() {
-    std::string str = "+ 4 + 6";
+    std::string str = "3 * 4 - (4 + 6)";
 
     std::vector<Token> tokens = tokenize(str);
     for(auto& token : tokens) {
